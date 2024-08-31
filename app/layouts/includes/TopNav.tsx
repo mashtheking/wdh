f37072e1d3d62bc18c370594565c48489f37957d
mashtheking <fredmaina174@gmail.com>
2024-08-31 09:42:33 +0300

    Update TopNav.tsx

--- app/layouts/includes/TopNav.tsx
+++ app/layouts/includes/TopNav.tsx
@@ -48,7 +48,7 @@ export default function TopNav() {
                 <div className={`flex items-center justify-between gap-6 w-full px-4 mx-auto ${pathname === '/' ? 'max-w-[1150px]' : ''}`}>
 
                     <Link href="/">
-                        <img className="min-w-[115px] w-[115px]" src="/images/tiktok-logo.png"/>
+                        <img className="min-w-[115px] w-[115px]" src="/images/mziki-logo.png"/>
                     </Link>
 
                     <div className="relative hidden md:flex items-center justify-end bg-[#F1F1F2] p-1 rounded-full max-w-[430px] w-full">
